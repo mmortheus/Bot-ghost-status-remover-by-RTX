acd0a9abdc865a007cddaa59eddfdfff50ee6bdd
mmortheus <159725759+mmortheus@users.noreply.github.com>
2024-02-11 21:58:38 +0200

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Watching Netflix","Listening to Spotify","Playing GTA VI"];
+const statusMessages = ["Dancing @Spirit Studio"];
 
 
 let currentIndex = 0;
